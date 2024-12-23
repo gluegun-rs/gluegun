69ccae654485812e9e876523fe7122cfc8d7d688
Niko Matsakis <nikomat@amazon.com>
2024-12-23 17:14:03 +0000

    WIP

---
+++ demos/hello_world/target/debug/deps/hello_world-e59cd56ce49e3e1d.d
@@ -0,0 +1,7 @@
+/workspaces/squared/demos/hello_world/target/debug/deps/libhello_world-e59cd56ce49e3e1d.rmeta: src/lib.rs
+
+/workspaces/squared/demos/hello_world/target/debug/deps/libhello_world-e59cd56ce49e3e1d.rlib: src/lib.rs
+
+/workspaces/squared/demos/hello_world/target/debug/deps/hello_world-e59cd56ce49e3e1d.d: src/lib.rs
+
+src/lib.rs:
---
+++ demos/hello_world/target/debug/libhello_world.d
@@ -0,0 +1,1 @@
+/workspaces/squared/demos/hello_world/target/debug/libhello_world.rlib: /workspaces/squared/demos/hello_world/src/lib.rs
