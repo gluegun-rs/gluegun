d0113ca4b49f89f81b820be43926b1cef227deb6
Niko Matsakis <nikomat@amazon.com>
2025-01-07 17:40:10 +0000

    eliminate warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse, parse_macro_input, ItemFn};
 
 /// `#[lingo_star::ignore]` has absolutely no effect but it is recognized by lingo* tooling
 #[proc_macro_attribute]
 pub fn ignore(attr: TokenStream, item: TokenStream) -> TokenStream {
-    parse_macro_input!(attr as syn::parse::Nothing);
+    syn::parse_macro_input!(attr as syn::parse::Nothing);
 
     item
 }
